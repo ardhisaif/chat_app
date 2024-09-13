046f6da127e8a9d5cf92969a6e6fc66c65a6537f
ardhihabel <ardhi.halobelanja@gmail.com>
2024-09-13 15:27:03 +0800

    add cable.js

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :room_messages,
+  dependent: :destroy
+
   def gravatar_url
     gravatar_id = Digest::MD5::hexdigest(email).downcase
     "https://gravatar.com/avatar/#{gravatar_id}.png"
